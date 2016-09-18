4e2e71a92031e782990c4c79105b2439f3b2922b
DiogoAzvg <diogo.azvg@gmail.com>
2016-09-18 03:23:44 -0300

    Configure in heroku vars

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,4 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-
-  config.omniauth :google_oauth2, ENV['GPLUS_KEY'], ENV['GPLUS_SECRET'], 
-    { access_type: "offline", approval_prompt: "" }
 end
