c9f389fb3933e76362591bf2984e83407185c436
DiogoAzvg <diogo.azvg@gmail.com>
2016-09-18 01:55:32 -0300

    Configure in heroku vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'travel-iff.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
@@ -68,6 +69,7 @@ Rails.application.configure do
     password: ENV["GMAIL_PASSWORD"],
     openssl_verify_mode: 'none'
   }
+  config.action_mailer.perform_deliveries = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
